48565a468eab3b3b89336f5aa8542180b6e6b17a
rflstruthers <rflstruthers@gmail.com>
2019-07-29 10:56:06 -0700

    Django Project

---
+++ techproject/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\user0\desktop\thetec~1\python\django\python~1\techpr~1\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
