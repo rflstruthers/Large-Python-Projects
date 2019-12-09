03c1d1cbaf611d3d0ec4c395f4ae00b6669962d1
rflstruthers <rflstruthers@gmail.com>
2019-12-09 10:56:21 -0800

    Folder update

--- techproject/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\user0\desktop\thetec~1\python\django\python~1\techpr~1\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
